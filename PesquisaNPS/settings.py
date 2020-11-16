5a9422f1fdc6cded75cd708ed5f3c6aa9a86d518
heldonjose <heldonjose@gmail.com>
2020-11-16 08:45:30 -0300

    configurações servidor

--- PesquisaNPS/settings.py
+++ PesquisaNPS/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -112,17 +112,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Recife'
+# USE_TZ = False, Se estiver em true o mysql recebe a data errada
+USE_TZ = False
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- PesquisaNPS/urls.py
+++ PesquisaNPS/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    path('', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
-    path('docs/', include_docs_urls(title='API de Documentação')),
-    path('controle_nps/', include('app_controle_nps.urls')),
+                  path('', admin.site.urls),
+                  # path('api-auth/', include('rest_framework.urls')),
+                  path('docs/', include_docs_urls(title='API de Documentação')),
+                  path('controle_nps/', include('app_controle_nps.urls')),
 
-]
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
